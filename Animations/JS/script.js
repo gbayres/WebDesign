c9da0ea33b6be8cbe7d3b00058571a684b9dffe8
gbayres <gbayres@hotmail.com>
2020-11-16 18:13:18 -0300

    Studies 16/11/20

--- Animations/JS/script.js
+++ Animations/JS/script.js
@@ -5,7 +5,7 @@ var mudar = document.getElementById("mudar");
 
 function f_passar(){
     var element = document.getElementById("titulo");
-    element.style.animation = "passar 1s linear infinite";
+    element.style.animation = "passar 5s linear infinite";
 
 }
 
@@ -17,7 +17,7 @@ function f_quicar(){
 
 function f_girar(){
     var element = document.getElementById("titulo");
-    element.style.animation = "girar 1s linear infinite";
+    element.style.animation = "girar 6s linear infinite";
 
 }
 
@@ -27,8 +27,40 @@ function f_mudar(){
 
 }
 
+function f_contar(e){
+    var max = e.target.maxLength;
+    var inside_text = e.target.value;
+    var $n_caracteres = document.getElementById("n_caracteres");
+    $n_caracteres.textContent = max - inside_text.length;
+
+}
+
+function f_termo(e){
+    var $termo = document.getElementById("termo");
+    var $botao_logar = document.getElementById("botao_logar");
+    
+    console.log($termo.checked);
+    
+    if ($termo.checked){
+        $botao_logar.disabled = false;
+    }
+
+    else {
+        $botao_logar.disabled = true;
+    }
+
+}
 
 passar.addEventListener("click", f_passar);
 quicar.addEventListener("click", f_quicar);
 girar.addEventListener("click", f_girar);
 mudar.addEventListener("click", f_mudar);
+
+var $n_caracteres = document.getElementById("n_caracteres")
+var $texto = document.getElementById("texto");
+$n_caracteres.textContent = $texto.getAttribute("maxlength");
+$texto.addEventListener("input", f_contar);
+
+var $termo = document.getElementById("termo")
+
+$termo.addEventListener("click", f_termo);
